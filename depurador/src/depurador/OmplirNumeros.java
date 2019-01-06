def9139fadeea5fc8db3af097a8d94281f2afeb3
aliciarossello <aliciarossello@192.168.1.43>
2019-01-06 22:28:45 +0100

    seguim provant ...

---
+++ depurador/src/depurador/OmplirNumeros.java
@@ -0,0 +1,28 @@
+package depurador;
+
+public class OmplirNumeros {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int[] taula = new int[n];
+		
+		taula = omplir(n);
+		int suma = sumar(taula);
+		System.out.println("La suma és: "+suma);
+	}
+
+	private static int[] omplir(int n) {
+		int[] tla = new int[n];
+		for (int i=0; i<n; i++) 
+			tla[i]=i*10;
+		return tla;
+	} // omplir taula
+	
+	private static int sumar(int[] tla) {
+		int suma = 0;
+		int n=tla.length;
+		for (int i=0; i<n; i++)
+			suma = suma + tla[i];
+		return suma;
+	} // sumar taula
+}
